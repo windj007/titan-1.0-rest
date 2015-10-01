a60da4a12bdc69c272b1332a466df68af350ae28
windj007 <windj007@gmail.com>
2015-10-01 17:59:21 +0300

    fix path and add a bit of logging

--- entry-point.sh
+++ entry-point.sh
@@ -3,13 +3,15 @@
 if [ -z "$CASSANDRA_HOST" ]; then
     echo "\$CASSANDRA_HOST is not set, using the default name (cassandra)"
 else
+    echo "Setting storage.hostname=$CASSANDRA_HOST"
     sed -i "s/storage.hostname=cassandra/storage.hostname=$CASSANDRA_HOST/" /srv/titan-1.0.0-hadoop1/conf/titan-cassandra-es.properties
 fi
 
 if [ -z "$ELASTICSEARCH_HOST" ]; then
     echo "\$ELASTICSEARCH_HOST is not set, using the default name (es)"
 else
-    sed -i "s/index.search.hostname=es/index.search.hostname=$ELASTICSEARCH_HOST/" /srv/titan-1.0.0-hadoop1/conf/gremlin-server/gremlin-server.yaml
+    echo "Setting index.search.hostname=$ELASTICSEARCH_HOST"
+    sed -i "s/index.search.hostname=es/index.search.hostname=$ELASTICSEARCH_HOST/" /srv/titan-1.0.0-hadoop1/conf/titan-cassandra-es.properties
 fi
 
 exec $@
